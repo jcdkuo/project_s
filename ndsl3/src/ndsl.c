c5fbc76a0b572032a970b5e7f43ab28cbc7fe700
Jerry Kuo <jcdkuo@gmail.com>
2020-05-08 16:26:31 +0800

    Let's come back to pointer
    - Remove conditional compilation
    - Move NSDL_Show into ndsl_init.c

--- ndsl3/src/ndsl.c
+++ ndsl3/src/ndsl.c
@@ -17,41 +17,16 @@
 
 #include "ndsl_common.h"
 
-static SCODE NDSL_Show(TNDSL *ptNDSL);
-
 int main()
 {
-    TNDSL   tNDSL;
-    SCODE   sReturn = S_OK;
+    TNDSL tNDSL;
+    SCODE sReturn = S_OK;
 
     // Initialization
     NDSL_Initialize(&tNDSL);
 
-    if (sReturn != S_OK) return ERROR_INITIALIZATION_FAILED;
-
-    // Show Info
-    sReturn = NDSL_Show(&tNDSL);
-    if (sReturn != S_OK) return ERROR_SHOW_MESSAGE_FAILED;
+    if (sReturn != S_OK)
+        return ERROR_INITIALIZATION_FAILED;
 
     return 0;
-}
-
-static SCODE NDSL_Show(TNDSL *ptNDSL)
-{
-    RETURN_SFAIL_IF_NULL(ptNDSL);
-    RETURN_SFAIL_IF(ptNDSL->iNDSLNum < 0);
-    
-    int iIndex;
-    
-    for (iIndex = 0; iIndex < ptNDSL->iNDSLNum; iIndex++)
-    {
-        printf("--- %d ---\n", (iIndex + 1));
-        printf("Name     : %s %s\n", ptNDSL->atPerson[iIndex].tName.acFirstName, ptNDSL->atPerson[iIndex].tName.acLastName);
-        printf("Age      : %d\n", ptNDSL->atPerson[iIndex].iAge);
-        printf("Job      : %s\n", ptNDSL->atPerson[iIndex].tTitle.acJobName);
-        printf("Seniority: %d\n", ptNDSL->atPerson[iIndex].tTitle.iSeniority);
-        printf("\n");
-    }
-    
-    return S_OK;
 }
--- ndsl3/src/ndsl_init.c
+++ ndsl3/src/ndsl_init.c
@@ -17,45 +17,40 @@
 
 #include "ndsl_common.h"
 
-#ifdef INITIAL_0
+/* Static Functions */
 static SCODE GetPersonByIndex(TPerson *ptPerson, int iIndex);
-#endif
-
-#ifndef INITIAL_2
 static SCODE GetNameByIndex(TName *ptName, int iIndex);
 static SCODE GetTitleByIndex(TTitle *ptTitle, int iIndex);
 static SCODE GetAgeByIndex(unsigned int *piAge, int iIndex);
-#endif
-
+static SCODE NDSL_Show(TNDSL *ptNDSL);
 
 TNnVPair g_atNDSLInfo[] = {
-    [CLASS_TEACHER]   = {"Jimmy",   "Chiu", 67},
-    [CLASS_SENIOR]    = {"Jerry",   "Kuo",  43},
-    [CLASS_JUNIOR]    = {"Sophie",  "Wang", 26},
-    [CLASS_TOTAL]     = {NULL,      NULL,   0},
+    [CLASS_TEACHER] = {"Jimmy", "Chiu", 67},
+    [CLASS_SENIOR] = {"Jerry", "Kuo", 43},
+    [CLASS_JUNIOR] = {"Sophie", "Wang", 26},
+    [CLASS_TOTAL] = {NULL, NULL, 0},
 };
 
 char *g_pszJobName[] = {
-    [CLASS_TEACHER]   = "Professor",
-    [CLASS_SENIOR]    = "Supervisor",
-    [CLASS_JUNIOR]    = "Engineer",
-    [CLASS_TOTAL]     = NULL,
+    [CLASS_TEACHER] = "Professor",
+    [CLASS_SENIOR] = "Supervisor",
+    [CLASS_JUNIOR] = "Engineer",
+    [CLASS_TOTAL] = NULL,
 };
 
 int g_iSeniority[] = {
-    [CLASS_TEACHER]   = 30,
-    [CLASS_SENIOR]    = 13,
-    [CLASS_JUNIOR]    = 2,
-    [CLASS_TOTAL]     = 0,   
+    [CLASS_TEACHER] = 30,
+    [CLASS_SENIOR] = 13,
+    [CLASS_JUNIOR] = 2,
+    [CLASS_TOTAL] = 0,
 };
 
-#ifdef INITIAL_0
 SCODE NDSL_Initialize(TNDSL *ptNDSL)
 {
     RETURN_SFAIL_IF_NULL(ptNDSL);
 
-    TNDSL   *ptInitNDSL = ptNDSL;
-    int     iNDSLIndex;
+    TNDSL *ptInitNDSL = ptNDSL;
+    int iNDSLIndex;
 
     // Sanitize
     memset(ptInitNDSL, 0, sizeof(TNDSL));
@@ -71,105 +66,14 @@ SCODE NDSL_Initialize(TNDSL *ptNDSL)
         // Person Count
         ptInitNDSL->iNDSLNum++;
         //printf("%s:%d iNDSLNum = %s\n", __func__, __LINE__, ptInitNDSL->iNDSLNum);
-
     }
+
+    // Show Info
+    RETURN_SFAIL_IF_NOT(NDSL_Show(ptInitNDSL) == S_OK);
 
     return S_OK;
 }
-#endif
 
-#ifdef INITIAL_1
-SCODE NDSL_Initialize(TNDSL *ptNDSL)
-{
-    RETURN_SFAIL_IF_NULL(ptNDSL);
-    
-    TNDSL   *ptInitNDSL = ptNDSL;
-    int     iNDSLIndex;
-
-    // Sanitize
-    memset(ptInitNDSL, 0, sizeof(TNDSL));
-
-    RETURN_SFAIL_IF(CLASS_TOTAL > MAX_PERSON_NUM);
-
-    printf("\n%s:%d\n\n", __func__, __LINE__);
-    for (iNDSLIndex = 0; iNDSLIndex < CLASS_TOTAL; iNDSLIndex++)
-    {
-        // Initialize
-        RETURN_SFAIL_IF_NOT(GetNameByIndex(&ptInitNDSL->atPerson[iNDSLIndex].tName, iNDSLIndex) == S_OK);
-        RETURN_SFAIL_IF_NOT(GetTitleByIndex(&ptInitNDSL->atPerson[iNDSLIndex].tTitle, iNDSLIndex) == S_OK);
-        RETURN_SFAIL_IF_NOT(GetAgeByIndex(&ptInitNDSL->atPerson[iNDSLIndex].iAge, iNDSLIndex) == S_OK);
-
-        // Person Count
-        ptInitNDSL->iNDSLNum++;
-        //printf("%s:%d iNDSLNum = %s\n", __func__, __LINE__, ptInitNDSL->iNDSLNum);
-
-    }
-
-    return S_OK;
-}
-#endif
-
-#ifdef INITIAL_2
-SCODE NDSL_Initialize(TNDSL *ptNDSL)
-{
-    RETURN_SFAIL_IF_NULL(ptNDSL);
-
-    TNDSL   *ptInitNDSL = ptNDSL;
-    int     iNDSLIndex;
-
-    // Sanitize
-    memset(ptInitNDSL, 0, sizeof(TNDSL));
-
-    RETURN_SFAIL_IF(CLASS_TOTAL > MAX_PERSON_NUM);
-
-#ifdef SOPHIE_VER
-    printf("\n%s:%d *** Sophie version ***\n\n", __func__, __LINE__);
-#else
-    printf("\n%s:%d *** Jerry version ***\n\n", __func__, __LINE__);
-#endif
-
-    for (iNDSLIndex = 0; iNDSLIndex < CLASS_TOTAL; iNDSLIndex++)
-    {
-        // Initialize
-#ifdef SOPHIE_VER
-
-        TPerson *ptPerson = &ptInitNDSL->atPerson[iNDSLIndex];
-
-        // Get Name
-        snprintf(ptPerson->tName.acFirstName, MAX_STRING_SIZE, "%s", g_atNDSLInfo[iNDSLIndex].cpcFirstName);
-        snprintf(ptPerson->tName.acLastName, MAX_STRING_SIZE, "%s", g_atNDSLInfo[iNDSLIndex].cpcLastName);
-
-        // Get Job Info
-        snprintf(ptPerson->tTitle.acJobName, MAX_STRING_SIZE, "%s", g_pszJobName[iNDSLIndex]);
-        ptPerson->tTitle.iSeniority = g_iSeniority[iNDSLIndex];
-
-        // Get Age
-        ptPerson->iAge = g_atNDSLInfo[iNDSLIndex].iAge;
-
-#else // JERRY_VER
-
-        // Get Name
-        snprintf(ptInitNDSL->atPerson[iNDSLIndex].tName.acFirstName, MAX_STRING_SIZE, "%s", g_atNDSLInfo[iNDSLIndex].cpcFirstName);
-        snprintf(ptInitNDSL->atPerson[iNDSLIndex].tName.acLastName, MAX_STRING_SIZE, "%s", g_atNDSLInfo[iNDSLIndex].cpcLastName);
-
-        // Get Job Info
-        snprintf(ptInitNDSL->atPerson[iNDSLIndex].tTitle.acJobName, MAX_STRING_SIZE, "%s", g_pszJobName[iNDSLIndex]);
-        ptInitNDSL->atPerson[iNDSLIndex].tTitle.iSeniority = g_iSeniority[iNDSLIndex];
-
-        // Get Age
-        ptInitNDSL->atPerson[iNDSLIndex].iAge = g_atNDSLInfo[iNDSLIndex].iAge;
-#endif
-        // Person Count
-        ptInitNDSL->iNDSLNum++;
-        //printf("%s:%d iNDSLNum = %s\n", __func__, __LINE__, ptInitNDSL->iNDSLNum);
-
-    }
-
-    return S_OK;
-}
-#endif
-
-#ifdef INITIAL_0
 static SCODE GetPersonByIndex(TPerson *ptPerson, int iIndex)
 {
     RETURN_SFAIL_IF_NULL(ptPerson);
@@ -181,9 +85,7 @@ static SCODE GetPersonByIndex(TPerson *ptPerson, int iIndex)
 
     return S_OK;
 }
-#endif
 
-#ifndef INITIAL_2
 static SCODE GetNameByIndex(TName *ptName, int iIndex)
 {
     RETURN_SFAIL_IF_NULL(ptName);
@@ -217,4 +119,23 @@ static SCODE GetAgeByIndex(unsigned int *piAge, int iIndex)
 
     return S_OK;
 }
-#endif
+
+static SCODE NDSL_Show(TNDSL *ptNDSL)
+{
+    RETURN_SFAIL_IF_NULL(ptNDSL);
+    RETURN_SFAIL_IF(ptNDSL->iNDSLNum < 0);
+
+    int iIndex;
+
+    for (iIndex = 0; iIndex < ptNDSL->iNDSLNum; iIndex++)
+    {
+        printf("--- %d ---\n", (iIndex + 1));
+        printf("Name     : %s %s\n", ptNDSL->atPerson[iIndex].tName.acFirstName, ptNDSL->atPerson[iIndex].tName.acLastName);
+        printf("Age      : %d\n", ptNDSL->atPerson[iIndex].iAge);
+        printf("Job      : %s\n", ptNDSL->atPerson[iIndex].tTitle.acJobName);
+        printf("Seniority: %d\n", ptNDSL->atPerson[iIndex].tTitle.iSeniority);
+        printf("\n");
+    }
+
+    return S_OK;
+}
